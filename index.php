d00cd1056bff99e2c97f90e8543ab22622a1a509
mahmudcodes <mahmudcodes@gmail.com>
2023-05-16 08:00:07 +0600

    number, variable

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    // echo "Mahmud Hasan";
+    // print "Hasan";
+    //var_dump ("web"); also print on screen but different way. 
+    // Number 
+    $x = 35;
+    $y = 10;
+    $addition = $x + $y;
+    $subtraction  = $x - $y;
+    $multiplication = $x * $y;
+    $division = $x / $y;
+    $remainder = $x % $y;
+    echo "Addition is = " . $addition ."<br> Subtraction is = ". $subtraction ."<br> Multiplication is = ". $multiplication ."<br> Division is = ". $division ."<br> Remainder is = ". $remainder . "<br>"; 
+    // String 
+    $personName = "Mahmud Hasan";
+    echo $personName;
+    // Boolean 
+    $flag = false;
+    echo $flag;
+?>
