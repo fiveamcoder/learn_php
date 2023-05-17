aa470b0f4a0ace25b24245983652b68f9b13b23c
mahmudcodes <mahmudcodes@gmail.com>
2023-05-17 09:31:53 +0600

    condition, some problems

--- index.php
+++ index.php
@@ -1,20 +1,53 @@
 <?php
-    // echo "Mahmud Hasan";
-    // print "Hasan";
-    //var_dump ("web"); also print on screen but different way. 
-    // Number 
-    $x = 35;
+    $break = "<br>";
+    $x = 0;
+    echo $x, $break;
+    ++$x  ; // pre increment operator
+    $x++ ;  // post increment operator
+    $x-- ;  // post decrement operator
+    $x = 2;
+    $y = 3;
+    echo $x**$y, $break; // ** Exponentiation arithmetic operator 
+
+    $x = 1;
+    $x += 8; // assignment operators
+    $x -= 2;
+    $x *= 2;
+    $x /= 2;
+    $x %= 2;
+    echo $x, $break; 
+
+    // Problem : Find greater number between two number
+
+    // Conditional Statements
+    $x = 7;
     $y = 10;
-    $addition = $x + $y;
-    $subtraction  = $x - $y;
-    $multiplication = $x * $y;
-    $division = $x / $y;
-    $remainder = $x % $y;
-    echo "Addition is = " . $addition ."<br> Subtraction is = ". $subtraction ."<br> Multiplication is = ". $multiplication ."<br> Division is = ". $division ."<br> Remainder is = ". $remainder . "<br>"; 
-    // String 
-    $personName = "Mahmud Hasan";
-    echo $personName;
-    // Boolean 
-    $flag = false;
-    echo $flag;
+    if($x > $y){ 
+    echo "A is Big";
+    }
+    else{
+        echo "B is Big" . $break;
+    }
+
+    // Problem : Check the number even or odd 
+
+    $x = 7;
+    if ($x % 2 == 0){
+        echo "Number is even";
+    }
+    else {
+        echo "Number is odd";
+    }
+
+    // Problem : Check the number positive or negative 
+
+    $x = -0.20;
+    if($x > 0 ){
+        echo $break . "Positive";
+    }
+    else{
+        echo $break . "Negative"; 
+    }
+    
+    echo "<p> Life is short </p>"
 ?>
